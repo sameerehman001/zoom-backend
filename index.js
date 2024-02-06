08f81b66ca42802e594a936db5a5d3e4fcee449e
Sameeurrehman <sameeurrehman@codistan.org>
2024-02-06 18:30:43 +0500

    disabled cors

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const corsOptions = [
   })
 ];
 app.use(bodyParser.json());
-app.use(corsOptions());
+// app.use(corsOptions());
+app.use(function(req,res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next();
+});
 
 app.post('/', (req, res) => {
 
